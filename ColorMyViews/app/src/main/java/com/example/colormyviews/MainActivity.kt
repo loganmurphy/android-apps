77e71ffc9c5e5509e6f5069ce2332063cd7b6ff4
loganmurphy <loganmurphy1984@gmail.com>
2020-06-01 23:15:40 -0500

    clear square colors when clicked a second time, work on adding an random background image

--- ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.colormyviews
 
 import android.graphics.Color
-import android.support.v7.app.AppCompatActivity
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
+import com.example.colormyviews.R.id.box_four
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+    val images: List<Int> = listOf(
+        R.drawable.image1_foreground,
+        R.drawable.image2_foreground,
+        R.drawable.image3_foreground,
+        R.drawable.image4_foreground,
+        R.drawable.image5_foreground
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         val boxOne = findViewById<TextView>(R.id.box_one)
         val boxTwo = findViewById<TextView>(R.id.box_two)
         val boxThree = findViewById<TextView>(R.id.box_three)
-        val boxFour = findViewById<TextView>(R.id.box_four)
+        val boxFour = findViewById<TextView>(box_four)
         val boxFive = findViewById<TextView>(R.id.box_five)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
@@ -28,7 +39,17 @@ class MainActivity : AppCompatActivity() {
         )
 
         for (view in clickableViews) {
-            view.setOnClickListener { makeColored(it) }
+            view.setOnClickListener {
+                val viewColor: Int? = if(it.background !== null) (it.background as ColorDrawable).color else null
+
+                when(viewColor) {
+                    -1 -> makeColored(it)
+                    else -> removeColor(it)
+                }
+
+
+//                addImage(it)
+            }
         }
     }
 
@@ -37,8 +58,14 @@ class MainActivity : AppCompatActivity() {
         R.id.box_one -> view.setBackgroundColor(Color.DKGRAY)
         R.id.box_two -> view.setBackgroundColor(Color.GRAY)
         R.id.box_three -> view.setBackgroundColor(Color.BLUE)
-        R.id.box_four -> view.setBackgroundColor(Color.MAGENTA)
+        box_four -> view.setBackgroundColor(Color.MAGENTA)
         R.id.box_five -> view.setBackgroundColor(Color.BLUE)
         else -> view.setBackgroundColor(Color.LTGRAY)
     }
+
+    private fun removeColor(view: View) = view.setBackgroundColor(Color.WHITE)
+
+    private fun addImage(view: View) { view.setBackgroundResource(getRandomImage())}
+
+    private fun getRandomImage() = images[Random.nextInt(4)]
 }
