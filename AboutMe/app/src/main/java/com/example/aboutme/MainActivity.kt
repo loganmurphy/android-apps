ad65079c5846456ed3eae0d97349b193ec95c626
loganmurphy <loganmurphy1984@gmail.com>
2020-06-04 20:31:43 -0500

    update data binding

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.aboutme.databinding.ActivityMainBinding
 
@@ -16,9 +16,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
 
         binding.apply {
-            myName = myName
             doneButton.setOnClickListener { addNickName(it) }
             nicknameText.setOnClickListener { updateNickname(it) }
         }
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.apply {
             myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
             nicknameEdit.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
 
